904c65202061be30c9cb4487b57cc635fc3bee98
Paul Krisadaolarn <pauly.k@gmail.com>
2021-08-10 17:17:21 +0700

    Added createPageSelector

--- src/lib/pageSelector.js
+++ src/lib/pageSelector.js
@@ -1,5 +1,33 @@
+import { writable } from "svelte/store"
 
+export function createPageSelector(total, limit) {
 
-function createPageSelector() {
-    
+    const state = {
+        total,
+        limit,
+        page: 1
+    }
+
+    const store = writable(state)
+
+    function nextPage() {
+        store.update(s => {
+            s.page = s.page < s.total ? s.page + 1 : s.page
+            return s
+        })
+    }
+
+    function previousPage() {
+        store.update(s => {
+            s.page = s.page > 1 ? s.page - 1 : s.page
+            return s
+        })
+
+    }
+
+    return {
+        pageSelector: store,
+        nextPage,
+        previousPage
+    }
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,14 @@
+<script>
+    import { createPageSelector } from "$lib/pageSelector";
+
+    let { pageSelector, nextPage, previousPage } = createPageSelector(25, 4);
+</script>
+
 <h1>Page Selector POC</h1>
+
+<button on:click={previousPage}>Prev</button>
+<button on:click={nextPage}>Next</button>
+
+<pre>
+    {JSON.stringify($pageSelector, null, 2)}
+</pre>
